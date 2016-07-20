cc51b4e8937665eba7e0a743216bbd538bb2219c
Israel Hofman <icehofman@hotmail.com>
2016-07-20 18:53:49 -0300

    Refactoring
    Refactoring model

--- src/main/java/com/icehofman/currency/quotation/api/models/CurrencyModel.java
+++ src/main/java/com/icehofman/currency/quotation/api/models/CurrencyModel.java
@@ -1,74 +1,28 @@
 package com.icehofman.currency.quotation.api.models;
 
 import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class CurrencyModel {
 
     private final String SEPARATOR = ";";
-
-    private Date date;
-    private Integer id;
-    private String type;
     private String name;
-    private BigDecimal buyingRate;
     private BigDecimal sellingRate;
-    private BigDecimal purchasingParity;
-    private BigDecimal saleParity;
 
     public CurrencyModel(String line) throws IllegalArgumentException {
         try {
             String[] data = line.split(SEPARATOR);
-
-            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-            this.date = formatter.parse(data[0]);
-            this.id = Integer.parseInt(data[1]);
-            this.type = data[2];
             this.name = data[3];
-            this.buyingRate = new BigDecimal(data[4].replace(",", "."));
             this.sellingRate = new BigDecimal(data[5].replace(",", "."));
-            this.purchasingParity = new BigDecimal(data[6].replace(",", "."));
-            this.saleParity = new BigDecimal(data[7].replace(",", "."));
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
-    }
-
-    public CurrencyModel() {
-        super();
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
     }
 
     public String getName() {
         return name;
     }
 
-    public BigDecimal getBuyingRate() {
-        return buyingRate;
-    }
-
     public BigDecimal getSellingRate() {
         return sellingRate;
-    }
-
-    public BigDecimal getPurchasingParity() {
-        return purchasingParity;
-    }
-
-    public BigDecimal getSaleParity() {
-        return saleParity;
     }
 }
