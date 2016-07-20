7ccffa838aecf6a3d3f921f8ab4141b2725b99f1
Israel Hofman <icehofman@hotmail.com>
2016-07-20 18:59:50 -0300

    Change
    Change tests names

--- src/test/java/com/icehofman/currency/quotation/api/QuotationTest.java
+++ src/test/java/com/icehofman/currency/quotation/api/QuotationTest.java
@@ -23,47 +23,47 @@ public class QuotationTest {
     }
 
     @Test(expected = IllegalValueException.class)
-    public void testCurrencyWithNegativeValue() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyWithNegativeValue() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         quotation.currencyQuotation("USD", "EUR", -1, "20/11/2014");
     }
 
     @Test(expected = IllegalDateException.class)
-    public void testCurrencyWithInvalidDate() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyWithInvalidDate() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         quotation.currencyQuotation("USD", "EUR", 100.00, "31/02/2014");
     }
 
     @Test(expected = NoExchangeRateForThisDateException.class)
-    public void testCurrencyWithoutExchangeRate() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyWithoutExchangeRate() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         quotation.currencyQuotation("USD", "EUR", 100.00, PeriodModel.getStringDate(PeriodModel.plusDays(new Date(), 2)));
     }
 
     @Test(expected = NonexistentCurrencyException.class)
-    public void testCurrencyWithoutExistentCurrencyOnFrom() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyWithoutExistentCurrencyOnFrom() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         quotation.currencyQuotation("YYY", "EUR", 100.00, "20/11/2014");
     }
 
     @Test(expected = NonexistentCurrencyException.class)
-    public void testCurrencyWithoutExistentCurrencyOnTo() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyWithoutExistentCurrencyOnTo() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         quotation.currencyQuotation("EUR", "ZZZ", 100.00, "20/11/2014");
     }
 
     @Test
-    public void testCurrencyQuotationUSDtoEUR() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyQuotationUSDtoEUR() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         Assert.assertEquals(new BigDecimal(79.69).setScale(2, RoundingMode.CEILING), quotation.currencyQuotation("USD", "EUR", 100.00, "20/11/2014"));
     }
 
     @Test
-    public void testCurrencyQuotationEURtoUSD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyQuotationEURtoUSD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         Assert.assertEquals(new BigDecimal(125.49).setScale(2, RoundingMode.CEILING), quotation.currencyQuotation("EUR", "USD", 100.00, "20/11/2014"));
     }
 
     @Test
-    public void testCurrencyQuotationNZDtoSBD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyQuotationNZDtoSBD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         Assert.assertEquals(new BigDecimal(563.70).setScale(2, RoundingMode.FLOOR), quotation.currencyQuotation("NZD", "SBD", 100.00, "20/11/2014"));
     }
 
     @Test
-    public void testCurrencyQuotationSBDtoNZD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
+    public void currencyQuotationSBDtoNZD() throws IllegalValueException, IllegalDateException, NoExchangeRateForThisDateException, NonexistentCurrencyException {
         Assert.assertEquals(new BigDecimal(18.58).setScale(2, RoundingMode.CEILING), quotation.currencyQuotation("SBD", "NZD", 100.00, "17/07/2016"));
     }
 }
--- src/test/java/com/icehofman/currency/quotation/api/models/PeriodModelTest.java
+++ src/test/java/com/icehofman/currency/quotation/api/models/PeriodModelTest.java
@@ -9,43 +9,43 @@ import java.util.Date;
 public class PeriodModelTest {
 
     @Test(expected = IllegalDateException.class)
-    public void testGetDateWithInvalidDate() throws IllegalDateException {
+    public void getDateWithInvalidDate() throws IllegalDateException {
         Assert.assertNull(PeriodModel.getDate("31/02/2016"));
     }
 
     @Test
-    public void testGetDate() throws IllegalDateException {
+    public void getDate() throws IllegalDateException {
         Assert.assertNotNull(PeriodModel.getDate("18/07/2016"));
     }
 
     @Test
-    public void testGetStringDate() throws IllegalDateException {
+    public void getStringDate() throws IllegalDateException {
         Assert.assertEquals("18/07/2016", PeriodModel.getStringDate(PeriodModel.getDate("18/07/2016")));
     }
 
     @Test
-    public void testgetDateWithDayOfWeekWhenIsSaturday() throws IllegalDateException {
+    public void getDateWithDayOfWeekWhenIsSaturday() throws IllegalDateException {
         Assert.assertEquals(PeriodModel.getDate("15/07/2016"), PeriodModel.getDateWithDayOfWeek(PeriodModel.getDate("16/07/2016")));
     }
 
     @Test
-    public void testgetDateWithDayOfWeekWhenIsSunday() throws IllegalDateException {
+    public void getDateWithDayOfWeekWhenIsSunday() throws IllegalDateException {
         Assert.assertEquals(PeriodModel.getDate("15/07/2016"), PeriodModel.getDateWithDayOfWeek(PeriodModel.getDate("15/07/2016")));
     }
 
     @SuppressWarnings("deprecation")
     @Test
-    public void testgetDateWithDayOfWeekWhenIsMonday() {
+    public void getDateWithDayOfWeekWhenIsMonday() {
         Assert.assertEquals(new Date(2016, 7, 9), PeriodModel.getDateWithDayOfWeek(new Date(2016, 7, 9)));
     }
 
     @Test
-    public void testMinusDays() throws IllegalDateException {
+    public void minusDays() throws IllegalDateException {
         Assert.assertEquals(PeriodModel.getDate("17/07/2016"), PeriodModel.minusDays(PeriodModel.getDate("18/07/2016"), 1));
     }
 
     @Test
-    public void testPlusDays() throws IllegalDateException {
+    public void plusDays() throws IllegalDateException {
         Assert.assertEquals(PeriodModel.getDate("17/07/2016"), PeriodModel.plusDays(PeriodModel.getDate("16/07/2016"), 1));
     }
 }
